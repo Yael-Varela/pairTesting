9b3636105bd093af8e363d1f2879bb8d49da0172
Yael Varela <garciayael715@gmail.com>
2026-05-24 16:57:16 -0600

    Test 1 - Tested and completed

--- Act2/tests/example.spec.ts
+++ Act2/tests/example.spec.ts
@@ -4,7 +4,9 @@ const pageURL = 'https://demo.playwright.dev/todomvc/#/'
 
 test('Add task',{tag : ['@add']},async ({ page }) => {
   await page.goto(pageURL);
-  
+
+  await page.evaluate(() => localStorage.clear());
+  await page.reload();
   //Locator 1. getByrole
   await expect(page.getByRole('heading', {name: 'todos'})).toBeVisible(); 
   
@@ -20,14 +22,14 @@ test('Add task',{tag : ['@add']},async ({ page }) => {
   await text.press('Enter');
   
   //Verify the creation of two tasks.
-  const list =  page.getByRole('listitem');
+  const list =  page.getByTestId('todo-item');
   await expect(list).toHaveCount(2);
 
   //Verify that the content of the tasks matches the ones created.
   await expect(list.nth(0)).toContainText('Testing homework');
   await expect(list.nth(1)).toContainText('See Mexico become world champion');
 
-  //The page counter should indicate that there are 2 tasks.
+  //The page counter for active task should indicate that there are 2 tasks.
   //Locator 3. getByTestId
   await expect (page.getByTestId('todo-count')).toContainText('2');
 });
@@ -36,11 +38,31 @@ test('Add task',{tag : ['@add']},async ({ page }) => {
 test('Complete tasks',{tag : ['@complete']},async ({ page }) => {
   test.slow();
   await page.goto(pageURL);
+  await page.evaluate(() => localStorage.clear());
+  await page.reload();
+
   const text = page.getByPlaceholder('What needs to be done?');
+  
   //Create a test task
-  await text.fill('Testing homework');
+  await text.fill('Active task: Testing homework');
   await text.press('Enter');
 
+  await text.fill('Completed task : See Mexico become world champion'); 
+  await text.press('Enter');
+  
+  //Locator 4. getByLabel
+  await page.getByLabel('Toggle Todo').nth(1).check();
+  //Only one remaining active task should stay
+  await expect.soft(page.getByTestId('todo-count')).toContainText('1');
+  //But we should expect to have 2 tasks (one completed and another one is active)
+  await expect.soft(page.getByRole('listitem')).toHaveCount(2);
+
+  //Clicking in the active tasks section
+  await page.getByRole('link', { name: 'Active' }).click();
+  
+  //Locator 5. getByText
+  await expect(page.getByText('Active task')).toBeVisible(); //The active task should be visible while the completed should not.
+  await expect(page.getByText('Completed task')).not.toBeVisible();
 });
 
 // test('t4', async ({ page }) => {
