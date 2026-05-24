95ddabcec1e3b8bfbffee7b99f7516dd950d7188
Yael Varela <garciayael715@gmail.com>
2026-05-24 17:33:19 -0600

    Test 2 - Tested and completed

--- Act2/tests/example.spec.ts
+++ Act2/tests/example.spec.ts
@@ -2,11 +2,9 @@ import { test, expect } from '@playwright/test';
 
 const pageURL = 'https://demo.playwright.dev/todomvc/#/'
 
-test('Add task',{tag : ['@add']},async ({ page }) => {
+test('T1: Add tasks',{tag : ['@add']},async ({ page }) => {
   await page.goto(pageURL);
 
-  await page.evaluate(() => localStorage.clear());
-  await page.reload();
   //Locator 1. getByrole
   await expect(page.getByRole('heading', {name: 'todos'})).toBeVisible(); 
   
@@ -35,11 +33,9 @@ test('Add task',{tag : ['@add']},async ({ page }) => {
 });
 
 
-test('Complete tasks',{tag : ['@complete']},async ({ page }) => {
+test('T2: Complete tasks',{tag : ['@complete']},async ({ page }) => {
   test.slow();
   await page.goto(pageURL);
-  await page.evaluate(() => localStorage.clear());
-  await page.reload();
 
   const text = page.getByPlaceholder('What needs to be done?');
   
@@ -54,8 +50,9 @@ test('Complete tasks',{tag : ['@complete']},async ({ page }) => {
   await page.getByLabel('Toggle Todo').nth(1).check();
   //Only one remaining active task should stay
   await expect.soft(page.getByTestId('todo-count')).toContainText('1');
+
   //But we should expect to have 2 tasks (one completed and another one is active)
-  await expect.soft(page.getByRole('listitem')).toHaveCount(2);
+  await expect.soft(page.getByTestId('todo-item')).toHaveCount(2);
 
   //Clicking in the active tasks section
   await page.getByRole('link', { name: 'Active' }).click();
